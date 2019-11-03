4bae36f99906637b258d309b610ebb947095eb13
Ulli Hafner <ullrich.hafner@gmail.com>
2019-11-03 23:24:46 +0100

    Initialize repository.

---
+++ src/main/java/io/jenkins/plugins/datatables/api/TableModel.java
@@ -0,0 +1,145 @@
+package io.jenkins.plugins.datatables.api;
+
+import java.util.List;
+import java.util.StringJoiner;
+
+/**
+ * Provides the model for the issues details table. The model consists of the following parts:
+ *
+ * <ul>
+ * <li>header name for each column</li>
+ * <li>width for each column</li>
+ * <li>content for each row</li>
+ * <li>content for whole table</li>
+ * </ul>
+ *
+ * @author Ullrich Hafner
+ */
+public abstract class TableModel {
+    public abstract String getId();
+
+    /**
+     * Returns the table header labels of the table.
+     *
+     * @return the table headers
+     */
+    public abstract List<String> getHeaders();
+
+    /**
+     * Returns the widths of the table columns.
+     *
+     * @return the width of the table columns
+     */
+    public abstract List<Integer> getWidths();
+
+    /**
+     * Returns the column definitions of the table.
+     *
+     * @return the column definitions
+     */
+    public final String getColumnsDefinition() {
+        ColumnDefinitionBuilder builder = new ColumnDefinitionBuilder();
+        configureColumns(builder);
+        return builder.toString();
+    }
+
+    /**
+     * Configures the columns of the report table.
+     *
+     * @param builder
+     *         the columns definition builder
+     */
+    protected abstract void configureColumns(ColumnDefinitionBuilder builder);
+
+    /**
+     * Returns the rows of the table.
+     *
+     * @return the rows
+     */
+    public abstract List<Object> getRows();
+
+    /**
+     * A JQuery DataTables column definition builder. Provides simple columns that extract a given entity property as
+     * column value or complex columns that provide different properties to sort and display a column.
+     */
+    public static class ColumnDefinitionBuilder {
+        private final StringJoiner columns = new StringJoiner(",", "[", "]");
+
+        /**
+         * Adds a new simple column that maps the specified property of the row entity to the column value.
+         *
+         * @param dataPropertyName
+         *         the property to extract from the entity, it will be shown as column value
+         *
+         * @return this
+         */
+        public ColumnDefinitionBuilder add(final String dataPropertyName) {
+            columns.add(String.format("{\"data\": \"%s\"}", dataPropertyName));
+
+            return this;
+        }
+
+        /**
+         * Adds a new complex column that maps the specified property of the row entity to the display and sort
+         * attributes of the column. The property {@code dataPropertyName} must be of type {@link
+         * DetailedColumnDefinition}.
+         *
+         * @param dataPropertyName
+         *         the property to extract from the entity, it will be shown as column value
+         * @param columnDataType
+         *         JQuery DataTables data type of the column
+         *
+         * @return this
+         * @see DetailedColumnDefinition
+         */
+        public ColumnDefinitionBuilder add(final String dataPropertyName, final String columnDataType) {
+            columns.add(String.format("{"
+                    + "  \"type\": \"%s\","
+                    + "  \"data\": \"%s\","
+                    + "  \"render\": {"
+                    + "     \"_\": \"display\","
+                    + "     \"sort\": \"sort\""
+                    + "  }"
+                    + "}", columnDataType, dataPropertyName));
+
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return columns.toString();
+        }
+    }
+
+    /**
+     * A column value attribute that provides a {@code display} and {@code sort} property so that a JQuery DataTables
+     * can use different properties to sort and display a column.
+     */
+    public static class DetailedColumnDefinition {
+        private final String display;
+        private final String sort;
+
+        /**
+         * Creates a new {@link DetailedColumnDefinition}.
+         *
+         * @param display
+         *         the entity property that should be used to display the column
+         * @param sort
+         *         the entity property that should be used to sort the column
+         *
+         * @see <a href="https://datatables.net/reference/option/columns.type">DataTables Column Types</a>
+         */
+        public DetailedColumnDefinition(final String display, final String sort) {
+            this.display = display;
+            this.sort = sort;
+        }
+
+        public String getDisplay() {
+            return display;
+        }
+
+        public String getSort() {
+            return sort;
+        }
+    }
+}
---
+++ src/main/webapp/js/table.js
@@ -0,0 +1,96 @@
+/* global jQuery, view */
+(function ($) {
+    const allTables = $('table.data-table');
+
+    allTables.each(function () {
+        const table = $(this);
+        const id = table.attr('id');
+
+        if (table.length) {
+            // Create a data table instance for the issues table.
+            const dataTable = table.DataTable({
+                language: {
+                    emptyTable: 'Loading - please wait ...'
+                },
+                deferRender: true,
+                pagingType: 'numbers', // Page number button only
+                order: [[1, 'asc']],
+                columnDefs: [{
+                    targets: 0, // First column contains details button
+                    orderable: false
+                }],
+                columns: JSON.parse(table.attr('data-columns-definition'))
+            });
+
+            // Add event listener for opening and closing details
+            table.on('click', 'div.details-control', function () {
+                const tr = $(this).parents('tr');
+                const row = dataTable.row(tr);
+
+                if (row.child.isShown()) {
+                    // This row is already open - close it
+                    row.child.hide();
+                    tr.removeClass('shown');
+                } else {
+                    // Open this row
+                    row.child($(this).data('description')).show();
+                    tr.addClass('shown');
+                }
+            });
+
+            table.on('becameVisible', function() {
+                // Content is loaded on demand: if the active tab shows the table, then content is loaded using Ajax
+                if (!table[0].hasAttribute('isLoaded')) {
+                    table.attr('isLoaded', 'true');
+                    view.getTableModel(id, function (t) {
+                        (function ($) {
+                            var model = JSON.parse(t.responseObject());
+                            dataTable.rows.add(model).draw();
+                        })(jQuery);
+                    });
+                }
+            });
+
+
+        }
+    });
+
+    allTables.on('order.dt', function (e) {
+        var table = $(e.target);
+        var order = table.DataTable().order();
+        var id = table.attr('id');
+        localStorage.setItem(id + '#orderBy', order[0][0]);
+        localStorage.setItem(id + '#orderDirection', order[0][1]);
+    });
+
+    /**
+     * Restores the order of every table by reading the local storage of the browser.
+     * If no order has been stored yet, the table is skipped.
+     * Also saves the default length of the number of table columns.
+     */
+    allTables.each(function () {
+        // Restore order
+        var id = $(this).attr('id');
+        var orderBy = localStorage.getItem(id + '#orderBy');
+        var orderDirection = localStorage.getItem(id + '#orderDirection');
+        var dataTable = $(this).DataTable();
+        if (orderBy && orderDirection) {
+            var order = [orderBy, orderDirection];
+            try {
+                dataTable.order(order).draw();
+            } catch (ignore) { // TODO: find a way to determine the number of columns here
+                dataTable.order([[1, 'asc']]).draw();
+            }
+        }
+        // Store paging size
+        $(this).on('length.dt', function (e, settings, len) {
+            localStorage.setItem(id + '#table-length', len);
+        });
+        var storedLength = localStorage.getItem(id + '#table-length');
+        if ($.isNumeric(storedLength)) {
+            dataTable.page.len(storedLength).draw();
+        }
+    });
+
+})(jQuery);
+
